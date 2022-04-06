2aefe3274270a2330cfe34f73c2f243eda24b5c1
Bryan <34401335+bryanc12@users.noreply.github.com>
2022-04-06 15:13:47 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,5 @@ apt-get -y install git
 rm -r /application/git
 mkdir /application/git
 git clone "$REPO" /application/git
-echo git
 chmod +x /application/git/app.sh
-echo chmod
 /application/git/app.sh
