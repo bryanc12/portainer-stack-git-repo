3db6c5b9304238389f59d71b96b7c4d430ac2b55
Bryan Choong <bryanchoong12@gmail.com>
2022-04-06 12:52:13 +0800

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install git python3 pip
+apt-get -y install git
 git clone $1 /application/git
 chmod +x /application/git/app.sh
 /application/git/app.sh
