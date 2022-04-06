27ca27d5241d1e7c5f68ad2ba3930da20c324bd8
Bryan Choong <bryanchoong12@gmail.com>
2022-04-06 11:47:49 +0800

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y install git
 echo "$REPO"
-pwd
-git clone $1 /application
-chmod +x /application/app.sh
-./application/app.sh
+git clone $1 /application/app
+chmod +x /application/app/app.sh
+./application/app/app.sh
